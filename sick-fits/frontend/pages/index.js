60531a159ad28efa0ba8987c5c91483d98cce18c
Francesc Busquets <francesc@gmail.com>
2018-10-28 09:28:34 +0100

    module1

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -4,11 +4,6 @@ import Link from 'next/link';
 const Home = props => (
   <div>
     <p>Hello world!</p>
-    <p>
-      <Link href="sell">
-        <a>Sell!</a>
-      </Link>
-    </p>
   </div>
 );
 
