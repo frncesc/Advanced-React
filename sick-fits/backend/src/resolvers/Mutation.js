af3f38479cda4aa3492c38dce9b312e3d641f73d
Francesc Busquets <francesc@gmail.com>
2018-11-05 23:21:54 +0100

    Unit 14

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,14 @@
-const Mutation = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info){
+    // TODO: Check if they are logged in
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+    console.log(item);
+    return item;
+  }
+};
 
-module.exports = Mutation;
+module.exports = Mutations;
