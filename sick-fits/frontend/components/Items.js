b5d0a52776a7177f6b60b51e24a15904e5190765
Francesc Busquets <francesc@gmail.com>
2018-11-12 23:20:53 +0100

    New Item component

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import Item from './Item';
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -9,6 +10,7 @@ const ALL_ITEMS_QUERY = gql`
       id
       title
       description
+      price
       image
       largeImage
     }
@@ -29,15 +31,15 @@ const ItemsList = styled.div`
 
 class Items extends Component {
   render() {
-    return(
+    return (
       <Center>
         <Query query={ALL_ITEMS_QUERY}>
-          {({data, error, loading})=>{
-             if(loading) return <p>Loading...</p>
-             if(error) return <p>Error: {error.message}</p>
-             return <ItemsList>
-               {data.items.map(item => <p>{item.title}</p>)}
-             </ItemsList>
+          {({ data, error, loading }) => {
+            if (loading) return <p>Loading...</p>
+            if (error) return <p>Error: {error.message}</p>
+            return <ItemsList>
+              {data.items.map(item => <Item item={item} key={item.id}></Item>)}
+            </ItemsList>
           }}
         </Query>
       </Center>
