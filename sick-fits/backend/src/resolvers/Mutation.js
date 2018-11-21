8329aeeb064ad3a5c35f01f70f07281ace18c66c
Francesc Busquets <francesc@gmail.com>
2018-11-21 19:27:25 +0100

    Updating items

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,5 +1,5 @@
 const Mutations = {
-  async createItem(parent, args, ctx, info){
+  async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
     const item = await ctx.db.mutation.createItem({
       data: {
@@ -8,7 +8,23 @@ const Mutations = {
     }, info);
     console.log(item);
     return item;
-  }
+  },
+  updateItem(parent, args, ctx, info) {
+    // first make a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Mutations;
