c6a94737dfb4e877ecc7b4f83d6e0ede5da505f8
Francesc Busquets <francesc@gmail.com>
2019-02-24 11:09:40 +0100

    Add my cart

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -35,7 +35,7 @@ class Items extends Component {
   render() {
     return (
       <Center>
-        <Pagination page={this.props.page}></Pagination>
+        <Pagination page={this.props.page || 1}></Pagination>
         <Query
           query={ALL_ITEMS_QUERY}
           //fetchPolicy="network-only"
